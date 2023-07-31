4f01a5f037eebb3bed5211e92534f77388311a6c
Alaabarry <zerohand99@gmail.com>
2023-07-31 03:08:40 +0300

    SimpleMathApp

---
+++ SimpeMathMethods/SimpeMathMethods/Math.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpeMathMethods
+{ 
+    //Create Class Math make it public to be accessable to other parts of program
+   public class Math
+    { 
+        // public method that execute math operation on two integers
+        public void myMath(int numOne, int numTwo) {
+            // Int numOne divided by 2 ,numTwo variable to display only
+            numOne /= 2; 
+            //Output Message of math operation and display NumTwo..
+            Console.WriteLine(" 10 Divided By 2 equal = "+numOne +", this Integer onlyfor display "+numTwo);
+        
+        }
+
+    }
+}
---
+++ SimpeMathMethods/SimpeMathMethods/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpeMathMethods
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        { 
+            // create object of class Math
+            Math sum = new Math(); 
+            //Call Mymath method and Pass Two Arguments to method to execute..
+            sum.myMath(numOne : 10 , numTwo :100);  
+            //required to do math operation on first number , the second number is only for display.. 
+            //Assigned method parameters by Name... 
+            Console.ReadLine();
+           
+
+        }
+    }
+}
