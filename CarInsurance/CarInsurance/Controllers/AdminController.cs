82b2a5cc6e39f61b91a2ad8cbec0085532c53b29
Alaabarry <zerohand99@gmail.com>
2023-08-11 20:35:51 +0300

    CarInsuranceQuote

---
+++ CarInsurance/CarInsurance/Controllers/AdminController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using CarInsurance.Models;
+using CarInsurance.ViewModel;
+
+namespace CarInsurance.Controllers
+{
+    public class AdminController : Controller
+    {
+        private InsuranceEntities db = new InsuranceEntities();
+
+        // GET: Admin
+        public ActionResult Index()
+        {
+            using (InsuranceEntities db = new InsuranceEntities())
+            {
+                var UserQuoteList = db.Insurees;
+                var quoteList = new List<ViewQuotes>();
+                foreach (var signup in UserQuoteList)
+                {
+                    var quote = new ViewQuotes();
+                    
+                    quote.FirstName = signup.FirstName;
+                    quote.LastName = signup.LastName; 
+                    quote.EmailAddress = signup.EmailAddress;
+                    quote.Quote = signup.Quote;
+                    quoteList.Add(quote);
+
+                }
+                return View(quoteList);
+
+            }
+
+
+        }
+    }
+}
+
---
+++ CarInsurance/CarInsurance/Controllers/InsureesController.cs
@@ -0,0 +1,148 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.UI;
+using CarInsurance.Models;
+
+namespace CarInsurance.Controllers
+{
+    public class InsureesController : Controller
+    {
+        private InsuranceEntities db = new InsuranceEntities();
+
+        // GET: Insurees
+        public ActionResult Index()
+        {
+            return View(db.Insurees.ToList());
+        }
+
+        // GET: Insurees/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Insuree insuree = db.Insurees.Find(id);
+            if (insuree == null)
+            {
+                return HttpNotFound();
+            }
+            return View(insuree);
+        }
+
+        // GET: Insurees/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Insurees/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id,FirstName,LastName,EmailAddress,DateOfBirth,CarYear,CarMake,CarModel,DUI,SpeedingTickets,CoverageType,Quote")] Insuree insuree)
+        {
+            if (ModelState.IsValid)
+            {
+                decimal CalcQuote = 50;
+                var UserAge = DateTime.Now.Year - insuree.DateOfBirth.Year;
+                if (UserAge <= 18) { CalcQuote += 100; }
+                if (UserAge >= 19 && UserAge <= 25) { CalcQuote += 50; }
+                if (UserAge >= 26) { CalcQuote += 25; }
+                if (insuree.CarYear <= 2000) { CalcQuote += 25; }
+                if (insuree.CarYear >= 2015) { CalcQuote += 25; }
+                if (insuree.CarMake.ToLower() == "porsche") { CalcQuote += 25; }
+                if (insuree.CarMake.ToLower() =="porsche"&& insuree.CarModel=="911 Carrera") { CalcQuote += 50; }
+                if (insuree.SpeedingTickets > 0) { CalcQuote = insuree.SpeedingTickets * 10 + CalcQuote; }
+                if (insuree.DUI == true) { CalcQuote *= 1.25m; }
+                if (insuree.CoverageType == true) { CalcQuote *= 1.5m; } 
+                insuree.Quote = CalcQuote; 
+                db.Insurees.Add(insuree); 
+               db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(insuree);
+        }
+
+        // GET: Insurees/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Insuree insuree = db.Insurees.Find(id);
+            if (insuree == null)
+            {
+                return HttpNotFound();
+            }
+            return View(insuree);
+        }
+
+        // POST: Insurees/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,FirstName,LastName,EmailAddress,DateOfBirth,CarYear,CarMake,CarModel,DUI,SpeedingTickets,CoverageType,Quote")] Insuree insuree)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(insuree).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(insuree);
+        }
+
+        // GET: Insurees/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Insuree insuree = db.Insurees.Find(id);
+            if (insuree == null)
+            {
+                return HttpNotFound();
+            }
+            return View(insuree);
+        }
+
+        // POST: Insurees/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Insuree insuree = db.Insurees.Find(id);
+            db.Insurees.Remove(insuree);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        
+       
+        }
+
+       
+
+    }
+
