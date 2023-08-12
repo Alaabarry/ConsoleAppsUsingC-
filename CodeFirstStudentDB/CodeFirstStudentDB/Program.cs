2ab8b54f86300a1c23cb962986c5a0817a0a8e46
Alaabarry <zerohand99@gmail.com>
2023-08-12 23:01:44 +0300

    CodeFirstStudentDB

---
+++ CodeFirstStudentDB/CodeFirstStudentDB/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeFirstStudentDB
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            using (var db = new SchoolContext())
+            {
+                var stud = new Student()
+                {
+                    StudentName = "Joe", 
+                    
+                };
+                db.Students.Add(stud);
+                db.SaveChanges();
+            }
+        }
+    }
+}
